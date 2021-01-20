3528fa8d78e4d6c3493eeaf4596098af2f0f83b3
linxiaozhou <1139904786@qq.com>
2021-01-20 09:56:02 +0800

    first commit

---
+++ src/test.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: linxiaozhou.com
+ * @LastEditors: linxiaozhou.com
+ * @Description: file content
+ */
+const welcome = (name: string): string => {
+  return `Hello ${name}`;
+}
+console.log(welcome.toString())
+console.log(welcome("linxiaozhou"))
